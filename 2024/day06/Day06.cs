87d16af5f28b1959781e7ca0d6efb528562bd725
Sanjay <itssanjay@gmail.com>
2024-12-07 12:33:05 -0500

    Day 6

--- 2024/day06/Day06.cs
+++ 2024/day06/Day06.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
+
 namespace AdventOfCode;
-using Map = char[][];
 
 public static class Day06
 {
@@ -13,45 +13,88 @@ public static class Day06
     {
         var map = ReadInput();
         var (pos, dir) = FindGuard(map);
-        while (pos.IsWithinMap(map))
+
+        while (pos?.IsWithinMap(map) == true)
         {
             map[pos.X][pos.Y] = 'X';
-            var nextPos = pos.Move(dir);
-            if (!nextPos.IsWithinMap(map))
-                break;
-            if (map[nextPos.X][nextPos.Y] == '#')
-                dir = dir.TakeRight();
-            else
-                pos = nextPos;
+            var nextPos = pos.Move(dir!);
+
+            if (!nextPos.IsWithinMap(map)) break;
+
+            pos = map[nextPos.X][nextPos.Y] == '#'
+                ? pos
+                : nextPos;
+            dir = map[nextPos.X][nextPos.Y] == '#'
+                ? dir.TakeRight()
+                : dir;
         }
 
-        var count = map.Sum(row => row.Count(ch => ch == 'X'));
-        
-        Console.WriteLine(count);
+        Console.WriteLine(map.Sum(row => row.Count(ch => ch == 'X')));
     }
 
     public static void SolvePart2()
     {
-        Console.WriteLine(0);
+        var map = ReadInput();
+        var (initialPos, initialDir) = FindGuard(map);
+
+        var count = (
+            from row in Enumerable.Range(0, map.Length)
+            from col in Enumerable.Range(0, map.Length)
+            where map[row][col] == '.'
+            let currMap = map.Select(r => r.ToArray()).ToArray()
+            where SimulatePath(currMap, row, col, initialPos!, initialDir!)
+            select 1
+        ).Count();
+
+        Console.WriteLine(count);
     }
 
-    private static (Position?, Direction?) FindGuard(Map map)
+    private static bool SimulatePath(char[][] map, int row, int col, Position pos, Direction dir)
     {
-        for (int i = 0; i < map.Length; i++)
-            for (int j = 0; j < map[i].Length; j++)
-                if (Direction.IsDirectionSymbol(map[i][j])) return (new Position(i, j), Direction.FromSymbol(map[i][j]));
+        map[row][col] = '#';
+        var visited = new HashSet<string>();
+        var curPos = pos;
+        var currDir = dir;
 
-        return (null, null);
+        while (curPos.IsWithinMap(map))
+        {
+            var visitKey = $"{curPos.X},{curPos.Y},{currDir.Symbol}";
+            if (!visited.Add(visitKey)) return true;
+
+            map[curPos.X][curPos.Y] = 'X';
+            var nextPos = curPos.Move(currDir);
+
+            if (!nextPos.IsWithinMap(map)) return false;
+
+            if (map[nextPos.X][nextPos.Y] == '#')
+            {
+                currDir = currDir.TakeRight();
+            }
+            else
+            {
+                curPos = nextPos;
+            }
+        }
+
+        return false;
     }
+
+    private static (Position?, Direction?) FindGuard(char[][] map) =>
+        (from row in Enumerable.Range(0, map.Length)
+         from col in Enumerable.Range(0, map[row].Length)
+         where Direction.IsDirectionSymbol(map[row][col])
+         select (new Position(row, col), Direction.FromSymbol(map[row][col])))
+        .FirstOrDefault((null, null));
 
     private record Position(int X, int Y)
     {
-        public Position Move(Direction dir) => new Position(X + dir.XOffset, Y + dir.YOffset);
-        public bool IsWithinMap(Map map) => X >= 0 && Y >= 0 && X < map.Length && Y < map.Length;
+        public Position Move(Direction dir) => new(X + dir.XOffset, Y + dir.YOffset);
+        public bool IsWithinMap(char[][] map) => X >= 0 && Y >= 0 && X < map.Length && Y < map[0].Length;
     }
+
     private record Direction(int XOffset, int YOffset, char Symbol, char rightSymbol)
     {
-        private static Dictionary<char, Direction> dirs = new Dictionary<char, Direction>()
+        private static readonly Dictionary<char, Direction> dirs = new()
         {
             {'^', new Direction(-1, 0, '^', '>') },
             {'<', new Direction(0, -1, '<', '^') },
@@ -61,8 +104,8 @@ public static class Day06
 
         public static bool IsDirectionSymbol(char symbol) => dirs.ContainsKey(symbol);
         public static Direction FromSymbol(char symbol) => dirs[symbol];
-        public Direction TakeRight() => dirs[this.rightSymbol];
+        public Direction TakeRight() => dirs[rightSymbol];
     }
 
-    private static Map ReadInput() => File.ReadAllLines(InputPath).Select(r => r.ToCharArray()).ToArray();
+    private static char[][] ReadInput() => File.ReadAllLines(InputPath).Select(r => r.ToCharArray()).ToArray();
 }
